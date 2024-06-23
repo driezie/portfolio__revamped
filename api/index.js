73cf0a288a72fc623c8991626a531b1b820c9829
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-06-23 19:38:34 +0200

    Push!

--- api/index.js
+++ api/index.js
@@ -14,11 +14,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
+
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 // use express with inject
 
-app.get('/', async (request, response) => {  
+
+
+
+app.get('/', async (request, response) => {
     response.render('index');
 });
 
@@ -38,7 +42,7 @@ app.get('/contact', async (request, response) => {
 // Projects
 
 // Routes om pagina's weer te geven 404
-const projectPages = ['packables-web-development', 'project_barzaarblox', 'project_old_portfolio', 'dda_dutch_digital_agency'];
+const projectPages = ['packables-web-development', 'project_barzaarblox', 'project_old_portfolio', 'dda_dutch_digital_agency', 'project_parcel_hub'];
 
 projectPages.forEach(page => {
   app.get(`/projects/${page}`, (req, res) => {
